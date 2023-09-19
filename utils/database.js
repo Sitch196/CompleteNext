ff7337420af6d05e119a3f70a7b35fac1982dbe2
Sitch196 <sichinava1996@gmail.com>
2023-09-19 18:52:03 +0400

    google auth with next-auth

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+let isConnected = false 
+export const connectToDB = async ()=> {
+    mongoose.set('strictQuery',true)
+
+if(isConnected){
+    console.log('Mongo is Connected')
+    return
+}
+
+try {
+    await mongoose.connect(process.env.MONGODB_URI,{
+        dbName:'Share_prompt',
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        
+    })
+
+    isConnected = true
+
+    console.log('MongoDB Connected')
+}catch(err){
+    console.log(err)
+}
+
+  
+}
